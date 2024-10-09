389db2943ac266970e31b9209b7b2ffc36a700d7
AndryAlexis <andry.html@gmail.com>
2024-10-09 22:32:26 +0200

    I have advanced doing the design for the aside menu

--- my-project/components/Connect4/index.jsx
+++ my-project/components/Connect4/index.jsx
@@ -65,12 +65,35 @@ const App = () => {
         toggleWinnerMenu={() => toggleWinnerMenu(winnerMenuRef)}
       />
       <aside className='h-full relative overflow-hidden shadow-md bg-[#f0f0ff]'>
-        <section className='m-4'>
-          <h1 className='text-2xl font-bold border-b-[1px] pb-2'>
-            Connect 4
-          </h1>
-          <form className="">
-
+        <section className='p-4 h-[inherit]'>
+          <form className="flex flex-col justify-between h-full">
+            <h1 className='text-2xl font-bold border-b-[1px] pb-2 mb-4'>
+              Connect 4
+            </h1>
+            <div className="flex flex-col justify-between h-[inherit]">
+              <div className='grid gap-6'>
+                <div className='grid gap-2'>
+                  <div className='border-2 rounded-md p-2 bg-[#fef9f9] flex items-center outline-offset-2 outline-4 outline-[#FADADD] hover:outline'>
+                    <input className='bg-transparent flex-1 w-full' type='text' placeholder='Player 1' />
+                    <select className='bg-transparent cursor-pointer'>
+                      <option value={'Rose'} selected>Rose</option>
+                      <option value={'Amber'}>Amber</option>
+                    </select>
+                  </div>
+                  <div className='border-2 rounded-md p-2 bg-[#fef9f9] flex items-center outline-offset-2 outline-4 outline-[#FADADD] hover:outline'>
+                    <input className='bg-transparent flex-1 w-full' type='text' placeholder='Player 2' />
+                    <select className='bg-transparent cursor-pointer'>
+                      <option value={'Rose'}>Rose</option>
+                      <option value={'Amber'} selected>Amber</option>
+                    </select>
+                  </div>
+                </div>
+                <button className="rounded-2xl w-full text-3xl font-bold bg-[#FFC0CB] hover:bg-[#ffb9c5] outline outline-offset-2 outline-4 outline-[#FADADD] hover:outline-[#f8d0d4] text-white">
+                  +
+                </button>
+              </div>
+              <input type="submit" value="START"/>
+            </div>
           </form>
         </section>
 
